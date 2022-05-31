c6cdd03da91b6b361817674cdc31e3487253c605
Vincent Engler <processoriented@gmail.com>
2022-05-30 21:42:58 -0500

    stubbing out RouteList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 // import Trip from './components/Trip';
-import Route from './components/Route';
+// import Route from './components/Route';
+import RouteList from './components/RouteList';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Route />
+      <RouteList />
     </div>
   );
 }
